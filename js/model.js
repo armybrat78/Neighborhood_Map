a3aab22368f6f26a225187e2882424121bd4d71e
Megan Dollar <megdollar@gmail.com>
2017-01-03 11:15:12 -0500

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,50 @@
+//Model of attractions
+ var model = [{
+         title: 'Kalalau Lookout',
+         fact: 'A valley that has beein in multiple films such as Jurrasic Park.',
+         location: {
+             lat: 22.1511297,
+             lng: -159.6459593
+         }
+     },
+     {
+         title: 'Glass Beach',
+         fact: 'A beach in Hanapepe made of sea glass.',
+         location: {
+             lat: 21.89813329999999,
+             lng: -159.584407
+         }
+     },
+     {
+         title: 'Anini Beach',
+         fact: 'A popular snorkeling site located on North Shore.',
+         location: {
+             lat: 22.2232101,
+             lng: -159.4629983
+         }
+     },
+     {
+         title: 'Na Pali Coast State Park',
+         fact: 'A park with hiking and beaches.',
+         location: {
+             lat: 22.1667456,
+             lng: -159.639244
+         }
+     },
+     {
+         title: 'Kalapaki Beach',
+         fact: 'Swimmer, surfers, and kayakers delight at this beach.',
+         location: {
+             lat: 21.9605229,
+             lng: -159.3501759
+         }
+     },
+     {
+         title: 'Kilauea Lighthouse',
+         fact: 'Located in the Kilauea Point National Wildlife Refuge.',
+         location: {
+             lat: 22.2316937,
+             lng: -159.4019597
+         }
+     }
+ ];
